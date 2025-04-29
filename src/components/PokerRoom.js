f441fd579ad7160932e9a028cb27e30878fe94a6
diego.freire <diego.freire@rpe.tech>
2025-04-29 16:08:08 -0300

    Ajustando para possibilitar personalizar cartas

--- src/components/PokerRoom.js
+++ src/components/PokerRoom.js
@@ -10,7 +10,8 @@ function PokerRoom() {
     const [votes, setVotes] = useState([]);
     const [average, setAverage] = useState(null);
 
-    const cards = Array.from({ length: 31 }, (_, i) => i * 2); // Números pares de 0 a 60
+    // const cards = Array.from({ length: 31 }, (_, i) => i * 2); // Números pares de 0 a 60
+    const cards = ['0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39', '?', '☕']; // Números pares de 0 a 60
 
     useEffect(() => {
         socket.on('updateUsers', ({ users }) => {
@@ -44,7 +45,7 @@ function PokerRoom() {
 
     return (
         <div>
-            <h2>ID da Sala: {roomId}</h2>
+            <h2>Sala: {roomId}</h2>
 
             <h3>Participantes:</h3>
             <ul>
