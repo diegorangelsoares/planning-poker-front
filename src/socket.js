be12d3221369071fdf7e7429fc71b09955b95371
diego.freire <diego.freire@rpe.tech>
2025-04-29 22:01:08 -0300

    --prod

--- src/socket.js
+++ src/socket.js
@@ -3,5 +3,4 @@ import { io } from 'socket.io-client';
 // const socket = io('http://localhost:4000'); // endereço do seu backend
 const socket = io('https://planning-poker-backend-xts0.onrender.com');
 
-
 export default socket;
